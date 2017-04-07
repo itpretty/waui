8955320726c2bebc01e861e8c9750cf3beedb3c1
lapulasi <liqtbcbh@163.com>
2017-04-07 17:46:33 +0800

    modify directory lever

--- js/wa_Component.js
+++ js/wa_Component.js
@@ -10,8 +10,6 @@ Vue.component('wa-searchbar', {
     methods:{
         //搜索
         searchInput: function() {
-            //alert(this.flag)
-            //window.open(this.logoData[this.flag].searchSrc+this.search);
             alert('hello worlds'+this.search);
         },
     }
@@ -23,31 +21,68 @@ Vue.component('wa-search-default', {
     template: '<div class="weui_navbar he-search">\
     <div class="frame frameScroll">\
     <form >\
-    <input name="textfield" type="text" id="textfield" placeholder="搜索你想找的商品名称"  class="hd_search_txt" v-model="searchContent">\
+    <input name="textfield" type="text" id="textfield" placeholder="搜索你想找的商品名称"  class="hd_search_txt" v-model="search">\
     <a class="hd_search_clear icon-ico27" href="javascript:;"  @click="clearInput()"></a>\
     </form>\
-    <div class="hd_button"><a href="search2.html" class="hd_search_btn_blue" >搜索</a></div>\
+    <div class="hd_button"><a :href="searchUrl" class="hd_search_btn_blue" @click="searchInput()">搜索</a></div>\
     </div>\
     </div>',
     data:function () {
-        searchContent:""
+        return{
+            search:"",
+            searchUrl:"search2.html"
+        }
     },
     methods:{
         //清除内容
         clearInput: function() {
-            alert( this.searchContent)
-            this.searchContent = '';
+            this.search = '';
             //this.get();
         },
         //搜索
         searchInput: function() {
-            //alert(this.flag)
-            //window.open(this.logoData[this.flag].searchSrc+this.search);
-            alert('hello worlds'+this.search);
+            if(this.searchUrl.indexOf("html")!=-1){//如果搜索是跳转页面的话
+                window.open(this.searchUrl);
+            }else{//不跳转页面在下面处理
+                alert('hello worlds'+this.search);
+            }
         },
     }
 
 })
+/*顶部搜索的组件 大麦场使用的搜索 有搜索按钮的*/
+Vue.component('wa-search-msg', {
+    template: '<div class="frame" style="top: 0px; position: absolute;">\
+    <a href="search.html">\
+    <form >\
+    <input name="textfield" type="text" id="textfield" placeholder="搜索你想找的商品名称"  class="hd_search_txt" v-model="search">\
+    </form></a>\
+    <div class="hd_me"> <a :href="searchUrl" @click="searchInput()"><span></span></a> </div>\
+    </div>',
+    data:function () {
+        return{
+            search:"",
+            searchUrl:"msg.html"
+        }
+    },
+    methods:{
+        //清除内容
+        clearInput: function() {
+            this.search = '';
+            //this.get();
+        },
+        //搜索
+        searchInput: function() {
+            if(this.searchUrl.indexOf("html")!=-1){//如果搜索是跳转页面的话
+                window.open(this.searchUrl);
+            }else{//不跳转页面在下面处理
+                alert('hello worlds'+this.search);
+            }
+        },
+    }
+
+})
+
 /*底部导航的组件*/
 Vue.component('wa-footer-nav', {
     template: '<div class="weui_tabbar bottom-menu" id="todo-list-item"><ul><li v-for="(todo,index) in todos"><a :href="todo.href" :class="{now: index==0}"><span class="titleico" :class="todo.class"></span><p>{{todo.navName}}</p></a></li></ul></div>',

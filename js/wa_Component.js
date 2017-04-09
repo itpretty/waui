fe90b09396ca555a298bfe32832769f272f97c0a
lapulasi <liqtbcbh@163.com>
2017-04-09 14:36:03 +0800

    about request data modify

--- js/wa_Component.js
+++ js/wa_Component.js
@@ -4,13 +4,13 @@
 /*顶部搜索的组件 weui 默认的样式  没有搜索按钮的*/
 Vue.component('wa-searchbar', {
     template: '<div class="weui-search-bar" id="searchBar"><form class="weui-search-bar__form"><div class="weui-search-bar__box"><i class="weui-icon-search" @click="searchInput()"></i><input type="search" class="weui-search-bar__input" id="searchInput" placeholder="搜索" required="" v-model="search"><a href="javascript:" class="weui-icon-clear" id="searchClear"></a></div><label class="weui-search-bar__label" id="searchText" style="transform-origin: 0px 0px 0px; opacity: 1; transform: scale(1, 1);"><i class="weui-icon-search"></i><span>搜索</span></label></form><a href="javascript:" class="weui-search-bar__cancel-btn" id="searchCancel">取消</a></div>',
-    data:function () {//组件里面的data必须为函数
+    data: function () {//组件里面的data必须为函数
         search:""
     },
-    methods:{
+    methods: {
         //搜索
-        searchInput: function() {
-            alert('hello worlds'+this.search);
+        searchInput: function () {
+            alert('hello worlds' + this.search);
         },
     }
 
@@ -21,30 +21,31 @@ Vue.component('wa-search-default', {
     template: '<div class="weui_navbar he-search">\
     <div class="frame frameScroll">\
     <form >\
-    <input name="textfield" type="text" id="textfield" placeholder="搜索你想找的商品名称"  class="hd_search_txt" v-model="search">\
+    <input name="textfield" type="text" id="textfield" :placeholder="text"  class="hd_search_txt" v-model="search">\
     <a class="hd_search_clear icon-ico27" href="javascript:;"  @click="clearInput()"></a>\
     </form>\
     <div class="hd_button"><a :href="searchUrl" class="hd_search_btn_blue" @click="searchInput()">搜索</a></div>\
     </div>\
     </div>',
-    data:function () {
-        return{
-            search:"",
-            searchUrl:"search2.html"
+    data: function () {
+        return {
+            search: "",
+            searchUrl: "search2.html"
         }
     },
-    methods:{
+    props: ["text"],
+    methods: {
         //清除内容
-        clearInput: function() {
+        clearInput: function () {
             this.search = '';
             //this.get();
         },
         //搜索
-        searchInput: function() {
-            if(this.searchUrl.indexOf("html")!=-1){//如果搜索是跳转页面的话
+        searchInput: function () {
+            if (this.searchUrl.indexOf("html") != -1) {//如果搜索是跳转页面的话
                 window.open(this.searchUrl);
-            }else{//不跳转页面在下面处理
-                alert('hello worlds'+this.search);
+            } else {//不跳转页面在下面处理
+                alert('hello worlds' + this.search);
             }
         },
     }
@@ -59,24 +60,24 @@ Vue.component('wa-search-msg', {
     </form></a>\
     <div class="hd_me"> <a :href="searchUrl" @click="searchInput()"><span></span></a> </div>\
     </div>',
-    data:function () {
-        return{
-            search:"",
-            searchUrl:"msg.html"
+    data: function () {
+        return {
+            search: "",
+            searchUrl: "msg.html"
         }
     },
-    methods:{
+    methods: {
         //清除内容
-        clearInput: function() {
+        clearInput: function () {
             this.search = '';
             //this.get();
         },
         //搜索
-        searchInput: function() {
-            if(this.searchUrl.indexOf("html")!=-1){//如果搜索是跳转页面的话
+        searchInput: function () {
+            if (this.searchUrl.indexOf("html") != -1) {//如果搜索是跳转页面的话
                 window.open(this.searchUrl);
-            }else{//不跳转页面在下面处理
-                alert('hello worlds'+this.search);
+            } else {//不跳转页面在下面处理
+                alert('hello worlds' + this.search);
             }
         },
     }
@@ -85,21 +86,32 @@ Vue.component('wa-search-msg', {
 
 /*底部导航的组件*/
 Vue.component('wa-footer-nav', {
-    template: '<div class="weui_tabbar bottom-menu" id="todo-list-item"><ul><li v-for="(todo,index) in todos"><a :href="todo.href" :class="{now: index==0}"><span class="titleico" :class="todo.class"></span><p>{{todo.navName}}</p></a></li></ul></div>',
-    data:function () {
-        return {
-            todos: [
-                {href: 'index.html', class: 'icon-ico8', navName: '首页'},
-                {href: 'classification.html', class: 'icon-ico9', navName: '分类'},
-                {href: 'http://www.baidu.com', class: 'icon-ico10', navName: '大麦场'},
-                {href: 'shoppingCar.html', class: 'icon-ico11', navName: '购物车'},
-                {href: 'login.html', class: 'icon-ico12', navName: '我的'}
-            ]
-        }
-    }
+    template: '<div class="weui_tabbar bottom-menu" id="todo-list-item"><ul><li v-for="(todo,index) in navList"><a :href="todo.href" :class="{now: index==0}"><span class="titleico" :class="todo.class"></span><p>{{todo.navName}}</p></a></li></ul></div>',
+    props: ["navList"],
+    methods: {}
 })
 
+
+//getData();
 /*创建根实例*/
 new Vue({
-    el:'#waUi'
+    el: '#waui',
+    data: {
+        todos: []
+    },
+    created: function () {
+        var self=this;
+        $.ajax({
+            type: 'GET',
+            url: '../data/tsconfig.json',
+            dataType: 'json',
+            success: function (requestdata) {
+                console.log(requestdata.todoList)
+                self.todos= requestdata.todoList;
+            },
+            error: function (xhr, type) {
+                alert('Ajax error!')
+            }
+        })
+    }
 })

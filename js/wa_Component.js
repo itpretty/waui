d03178d0a4d81c467f0897cd43c31eb426907693
lapulasi <liqtbcbh@163.com>
2017-04-11 11:18:51 +0800

    add page/wa_login.html

--- js/wa_Component.js
+++ js/wa_Component.js
@@ -15,42 +15,6 @@ Vue.component('wa-searchbar', {
     }
 
 })
-
-/*顶部搜索的组件 大麦场使用的搜索 有搜索按钮的*/
-Vue.component('wa-search-default', {
-    template: '<div class="weui_navbar he-search">\
-    <div class="frame frameScroll">\
-    <form >\
-    <input name="textfield" type="text" id="textfield" :placeholder="text"  class="hd_search_txt" v-model="search">\
-    <a class="hd_search_clear icon-ico27" href="javascript:;"  @click="clearInput()"></a>\
-    </form>\
-    <div class="hd_button"><a :href="searchUrl" class="hd_search_btn_blue" @click="searchInput()">搜索</a></div>\
-    </div>\
-    </div>',
-    data: function () {
-        return {
-            search: "",
-            searchUrl: "search2.html"
-        }
-    },
-    props: ["text"],
-    methods: {
-        //清除内容
-        clearInput: function () {
-            this.search = '';
-            //this.get();
-        },
-        //搜索
-        searchInput: function () {
-            if (this.searchUrl.indexOf("html") != -1) {//如果搜索是跳转页面的话
-                window.open(this.searchUrl);
-            } else {//不跳转页面在下面处理
-                alert('hello worlds' + this.search);
-            }
-        },
-    }
-
-})
 /*顶部搜索的组件 大麦场使用的搜索 有搜索按钮的*/
 Vue.component('wa-search-msg', {
     template: '<div class="frame" style="top: 0px; position: absolute;">\
@@ -83,35 +47,99 @@ Vue.component('wa-search-msg', {
     }
 
 })
+/*顶部搜索的组件 大麦场使用的搜索 有搜索按钮的*/
+Vue.component('wa-search-default', {
+    template: '<div class="weui_navbar he-search">\
+    <div class="frame frameScroll">\
+    <form >\
+    <input name="textfield" type="text" id="textfield" :placeholder="text"  class="hd_search_txt" v-model="search">\
+    <a class="hd_search_clear icon-ico27" href="javascript:;"  @click="clearInput()"></a>\
+    </form>\
+    <div class="hd_button"><a href="javascript:;" class="hd_search_btn_blue" @click="searchInput()">搜索</a></div>\
+    </div>\
+    </div>',
+    data: function () {
+        return {
+            search: "",
+        }
+    },
+    props: ["text","goPage","callBack"],
+    methods: {
+        //清除内容
+        clearInput: function () {
+            this.search = '';
+            //this.get();
+        },
+        //搜索
+        searchInput: function () {
+            //alert( this.goPage == "null")
+            if (this.goPage!="null") {//如果搜索是跳转页面的话
+                if(!window.localStorage){
+                    console.log("浏览器不支持localstorage");
+                    return false;
+                }else{
+                    var storage=window.localStorage;
+                    storage["searchContent"]=this.search;//保存搜索字段，在下个页面获取
+                    console.log(storage["searchContent"]);
 
-/*底部导航的组件*/
-Vue.component('wa-footer-nav', {
-    template: '<div class="weui_tabbar bottom-menu" id="todo-list-item"><ul><li v-for="(todo,index) in navList"><a :href="todo.href" :class="{now: index==0}"><span class="titleico" :class="todo.class"></span><p>{{todo.navName}}</p></a></li></ul></div>',
-    props: ["navList"],
-    methods: {}
+                }
+                window.open(this.goPage);
+            } else {//不跳转页面就去执行@click.native绑定的原生方法
+                console.log('Execution native method!');
+            }
+        },
+    }
+
 })
 
 
-//getData();
+/*底部导航的组件*/
+Vue.component('wa-footer-nav', {
+    template: '<div class="weui_tabbar bottom-menu" id="todo-list-item"><ul><li v-for="(todo,index) in navList"><a href="javascript:;" :class="{now: index==0}" @click="goPage(index)"><span class="titleico" :class="todo.class"></span><p>{{todo.navName}}</p></a></li></ul></div>',
+    props: ["navList"],
+    methods: {
+        goPage:function (index) {
+            alert(this.navList[index].href)
+            if(this.navList[index].href!="null"){//如果有跳转地址
+                window.open(this.navList[index].href);
+            }else{//如果不跳转执行原生方法
+                console.log("Execution native method")
+               // add(index);
+            }
+        }
+    }
+})
+
 /*创建根实例*/
 new Vue({
     el: '#waui',
     data: {
-        todos: []
+        footerData: [],//底部导航的数据
+        tId:0
+    },
+    methods:{
+        waNativeSearch:function () {//定义搜索按钮的原生方法
+            console.log("doSomething  content");
+        },
+        waFooterNavGo:function () {//底部导航不跳转的原生方法
+            console.log("do something ...");
+        }
     },
     created: function () {
         var self=this;
         $.ajax({
             type: 'GET',
-            url: '../data/tsconfig.json',
+            url: '../data/footerNavData.json',//底部导航的数据文件地址
             dataType: 'json',
             success: function (requestdata) {
                 console.log(requestdata.todoList)
-                self.todos= requestdata.todoList;
+                self.footerData= requestdata.todoList;
             },
             error: function (xhr, type) {
-                alert('Ajax error!')
+                alert('Ajax error fff!')
             }
         })
     }
 })
+
+

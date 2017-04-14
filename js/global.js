8aeb74bb06ff7235dbc1a750120c9196e0664489
riting.liu@ffzxnet.com <riting.liu@ffzxnet.com>
2017-04-14 11:21:06 +0800

    Re-write wa.path.root()

--- js/global.js
+++ js/global.js
@@ -1,18 +1,35 @@
+// Namespace
 var wa = {};
 
 // Path
 wa.path = {
   root: function(){
-    var ret = '/';    
-    if (window.location.host.indexOf('192.168.5.41') > -1) {
-      ret = '/waui/';
-    }
-    if (window.location.host.indexOf('localhost:63342') > -1) {
-      ret = '/new_waui/';
-    }
-    return ret;    
+    var ret = '/';
+    var map = [
+      {
+        // Riting Liu
+        host: '192.168.5.41',
+        root: '/waui/'
+      },
+      {
+        // Qiaoting Li
+        host: 'localhost:63342',
+        root: '/new_waui/'
+      }
+    ];
+    var mapping = function(){
+      var host = window.location.host;
+      $.each(map, function(key, val){
+        if (host.indexOf(val.host) > -1) {
+          ret = val.root;
+          return false;
+        }
+      });
+      return ret;
+    };
+    return mapping();    
   }
-}
+};
 
 $(function() {
     FastClick.attach(document.body);
